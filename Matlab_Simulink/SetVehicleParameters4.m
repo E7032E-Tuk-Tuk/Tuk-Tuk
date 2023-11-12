cc2090b31549a8620ccc35de3da1ad44fc2df336
Magnus Norén <amuono-1@student.ltu.se>
2023-11-12 22:30:09 +0100

    TV on the latest FMU (individual wheel parameters)

--- Matlab_Simulink/SetVehicleParameters4.m
+++ Matlab_Simulink/SetVehicleParameters4.m
@@ -13,10 +13,40 @@ h = 0.05; % Height of COM
 
 gearRatio = 7;
 
-wheelRadius = 0.28;
-wheelIyy = 0.3;
-rollingResistanceCoeff = 0.0;
+% Front tire
+wheelRadiusF = 0.28;
+inertiaF = 0.3;
+rollResF = 0.0;
+muAF = 0.8;
+muSF = 0.7;
+sAdF = 0.15;
+sSlF = 0.2;
+vAdF = 0.1;
+vSlF = 0.3;
 
+% Rear left tire
+wheelRadiusL = 0.28;
+inertiaL = 0.3;
+rollResL = 0.0;
+muAL = 1.0;
+muSL = 0.9;
+sAdL = 0.15;
+sSlL = 0.2;
+vAdL = 0.1;
+vSlL = 0.3;
+
+% Rear right tire
+wheelRadiusR = 0.28;
+inertiaR = 0.3;
+rollResR = 0.0;
+muAR = 1.0;
+muSR = 0.9;
+sAdR = 0.15;
+sSlR = 0.2;
+vAdR = 0.1;
+vSlR = 0.3;
+
+%%
 c_f_nom = 12e3; % Front tires cornering stiffness (at nominal vertical load)
 c_r_nom = 15e3; % Rear tires cornering stiffness (at nominal vertical load)
 Fznom = 5000; % Nominal vertical load
