8510ec0c20f656248008c93358c8005870549dac
Magnus Norén <amuono-1@student.ltu.se>
2023-12-14 15:00:52 +0100

    Made some changes to TV_controller.c

--- Source code/TV_controller.c
+++ Source code/TV_controller.c
@@ -37,7 +37,8 @@
 
 // Misc constants
 #define Ts = ??? // Sampling time
-#define MaxCMTorqueFraction 0.8 // Fraction of the maximum available torque to be used for common mode when at full throttle?
+#define MaxCMTorqueFraction 0.8 // Fraction of the maximum available torque to be used for common mode when at full throttle
+#define MinVelLong = 0.05 // Minimum longitudinal velocity to trigger anti-slip system. Avoids division by zero
 
 // ------------------------------------------------------------------------
 // Global variables (ikr...)
@@ -52,7 +53,7 @@ float angVelMotorRight = 0;
 float angVelWheelLeft = 0;
 float angVelWheelRight = 0;
 
-float accInput = 0; // Accelerator input (Here I'm assuming a value between 0 and 1
+float accInput = 0; // Accelerator input (Here I'm assuming a value between 0 and 1)
 
 // Variables for the Yaw rate PI controller
 float yawTorqueRef = 0;
@@ -109,7 +110,7 @@ void yawRateController()
 	float yawRateRef = velLong * tan(steerAngleMeas) / WheelBase;
 	float error = yawTorqueRef - yawRate;
 	
-	// PI controller in velocity form
+	// PI controller in incremental form (aka "velocity form")
 	yawTorqueRef = previousYawTorqueRef + Kp*(error - previousError) + Ki*Ts*error;
 	previousError = error;
 }	
@@ -126,28 +127,34 @@ void torqueVectoring()
 	motorTorqueLeftRef = fmin(maxMotorTorqueLeft, fmax(motorTorqueLeftRef, minMotorTorqueLeft));
 	motorTorqueRightRef = fmin(maxMotorTorqueRight, fmax(motorTorqueRightRef, minMotorTorqueRight));
 
-	// Report the saturated value back to the yaw rate PI as the previous output, which should prevent integrator windup since it's in velocity form
-	saturatedDM = (motorTorqueLeftRef - motorTorqueRightRef) / 2; // Again, flip the signs here if using CCW yaw
+	// Report the saturated value back to the yaw rate PI as the previous output, which should prevent integrator windup since it's in incremental form
+	float saturatedDM = (motorTorqueLeftRef - motorTorqueRightRef) / 2; // Again, flip the signs here if using CCW yaw
 	previousYawTorqueRef = saturatedDM * GearRatio * TrackWidth / WheelRadiusLoaded;
 }
 
 void antiSpin()
 {
 	// This code aims to limit the slip ratios below about 0.1 in absolute value to prevent wheel spin and loss of grip
-
-	float slipRatioLeft = (angVelWheelLeft * WheelRadiusEffective - velLong) / velLong;
-	float slipRatioRight = (angVelWheelRight * WheelRadiusEffective - velLong) / velLong;
-
-	float limitFractionLeft = exp(-B_lambda * (fabs(slipRatioLeft) - A_lambda));
-	limitFractionLeft = fmin(limitFractionLeft, 1);
-	float limitFractionRight = exp(-B_lambda * (fabs(slipRatioRight) - A_lambda));
-	limitFractionRight = fmin(limitFractionRight, 1);
 	
-	motorTorqueLeftRef = limitFractionLeft * motorTorqueLeftRef;
-	motorTorqueRightRef = limitFractionRight * motorTorqueRightRef;
+	if (velLong > MinVelLong){		
+		float slipRatioLeft = (angVelWheelLeft * WheelRadiusEffective - velLong) / velLong;
+		float slipRatioRight = (angVelWheelRight * WheelRadiusEffective - velLong) / velLong;
+		
+		float limitFractionLeft = exp(-B_lambda * (fabs(slipRatioLeft) - A_lambda));
+		limitFractionLeft = fmin(limitFractionLeft, 1);
+		float limitFractionRight = exp(-B_lambda * (fabs(slipRatioRight) - A_lambda));
+		limitFractionRight = fmin(limitFractionRight, 1);
+	
+		motorTorqueLeftRef = limitFractionLeft * motorTorqueLeftRef;
+		motorTorqueRightRef = limitFractionRight * motorTorqueRightRef;
+	}		
+	
 	// NOTE: In principle we could recalculate previousYawTorqueRef here instead of in the torqueVectoring step,
 	// but at least in the simulations it seems fine to keep integrating when the anti-spin kicks in.
 	// Change this if necessary, but be aware it could cause unwanted behaviour since the anti-spin can limit the output quite aggressively.
+	
+	// Update: New simulation indicates that it might be benificial to do the recalculation here after all. Best to try both ways.
+	// To change, move the last two lines from torqueVectoring() here.
 }	
 
 // Main function here:
@@ -165,7 +172,7 @@ void calculateMotorTorqueRef()
 	getMotorTorqueLimits();
 	
 	// Determine common mode torque
-	float torqueCM = accInput * MaxCMTorqueFraction * fmax(maxMotorTorqueLeft, maxMotorTorqueRight);
+	float torqueCM = accInput * MaxCMTorqueFraction * fmin(maxMotorTorqueLeft, maxMotorTorqueRight);
 	
 	yawRateController();
 	torqueVectoring();
